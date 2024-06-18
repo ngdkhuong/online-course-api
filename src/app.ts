44db64172c9863fefcdb01d195579f152f54ee19
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-06-18 23:08:19 +0700

    deploy to ec2

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: 'API is working',
+    });
+});
