4a7a033f92913818ddf54caac5d3886ff8c95926
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-07-09 15:44:40 +0700

    deleted: system old files

--- src/Config/index.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config({
-    path: `.env.${process.env.NODE_ENV || 'development'}.local`,
-});
-
-export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-
-export const { MONGO_URI } = process.env;
