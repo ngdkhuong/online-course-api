21a92c3648a6b9cdd0e7066496902796b3fe6a3f
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-07-21 14:53:37 +0700

    upload: cd workflow

--- jest.config.js
+++ jest.config.js
@@ -16,5 +16,8 @@ module.exports = {
         '^@constants/(.*)$': '<rootDir>/src/constants/$1',
         '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
         '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+        '^@models/(.*)$': '<rootDir>/src/models/$1',
+        '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+        '^@services/(.*)$': '<rootDir>/src/services/$1',
     },
 };
