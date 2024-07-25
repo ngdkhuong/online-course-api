ae87755b5fce53d573900ce815220e95cb5eba3f
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-07-25 17:15:51 +0700

    testing api ec2

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
-import AuthController from '@controllers/auth.controller';
+// import { Router } from 'express';
+// import AuthController from '../controllers/auth.controller';
 
-const router = Router();
+// const router = Router();
 
-router.post('/register', AuthController.register);
-router.post('/login', AuthController.login);
-router.get('/google', AuthController.googleAuth);
-router.get('/google/callback', AuthController.googleAuthCallback);
-router.get('/github', AuthController.githubAuth);
-router.get('/github/callback', AuthController.githubAuthCallback);
+// router.post('/register', AuthController.register);
+// router.post('/login', AuthController.login);
+// router.get('/google', AuthController.googleAuth);
+// router.get('/google/callback', AuthController.googleAuthCallback);
+// router.get('/github', AuthController.githubAuth);
+// router.get('/github/callback', AuthController.githubAuthCallback);
 
-export default router;
+// export default router;
